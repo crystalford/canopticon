36610f392ea685a36ccae10fef128ebb1e62f836
crystalford <chrisrobtelford@gmail.com>
2026-01-12 14:37:13 -0500

    Fix: Add missing 'use client' directive to TrendStream.tsx

--- components/TrendStream.tsx
+++ components/TrendStream.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useState, useEffect } from 'react'
 import { Trend } from '@/types'
 import { ingestTrendsAction, getTrendsAction, generateTrendResponseAction } from '@/app/actions'
